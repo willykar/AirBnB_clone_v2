51d771488a9a9f971f61b267d7f2ee3b778f33ba
willykar <wilsonnjenga799@gmail.com>
2024-03-20 14:26:00 -0700

    update

--- models/user.py
+++ models/user.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
+import models
 from sqlalchemy.ext.declarative import declarative_base
 
 
