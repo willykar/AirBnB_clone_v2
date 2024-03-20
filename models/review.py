4b45a8ef10e39a4d307c4829505d4e21133c9144
willykar <wilsonnjenga799@gmail.com>
2024-03-20 14:31:24 -0700

    update

--- models/review.py
+++ models/review.py
@@ -2,11 +2,12 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review class store review information """
-    __tablenam__ = "reviews"
+    __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"),
                       nullable=False)
