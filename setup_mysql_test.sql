caf92ec76b9fd5b81108d6f3bc94420b3a0f06b7
willykar <wilsonnjenga799@gmail.com>
2024-03-20 06:10:15 -0700

    update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 -- Prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
-=======
--- Prepares a MySQL Test server for the project.
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
->>>>>>> e1c784a55e4229df3956360db3d27d6e5e4ab369
---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""test_db_storage module"""
+import pep8
+import unittest
+import json
+from os import getenv
+from models.base_model import BaseModel, Base
