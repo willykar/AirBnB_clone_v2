e885cc57db7a41fdde2d5afae4b20eb3480761f7
willykar <wilsonnjenga799@gmail.com>
2024-03-20 03:56:45 -0700

    update

--- models/place.py
+++ models/place.py
@@ -2,11 +2,13 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
-from sqlalchemy import ForeignKey, Float
+from sqlalchemy import ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+import models
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ This class Place
     Attributes:
         city_id: string
@@ -20,11 +22,11 @@ class Place(BaseModel):
         lattitude: integer
         longitude: float
     """
-    __tablname__ = "places"
-    city_id = Column(String(60), nullable=False,
-                     ForeignKey('cities.id'))
-    user_id = Column(String(60), nullable=False,
-                     Foreignkey('users.id'))
+    __tablename__ = "places"
+    city_id = Column(String(60),
+                     ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'),
+                     nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import Place
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class User(BaseModel, Base):
