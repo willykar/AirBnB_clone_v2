4aa198c8901903ad910c8a9de934b4b75ac91efd
willykar <wilsonnjenga799@gmail.com>
2024-03-20 01:47:15 -0700

    update file

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
@@ -12,3 +13,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", cascade='all, delete,
+                          delete-orphan', backref='cities')
--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -12,3 +13,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place",
+                          cascade='all, delete, delete-orphan', backref="user")
