da0cc2642a01d12d62aaea3264c3b8e5b8f0b44f
willykar <wilsonnjenga799@gmail.com>
2024-03-21 07:05:27 -0700

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel. Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Table
 from sqlachemy.orm import relationship
 from models.place import place_amenity
