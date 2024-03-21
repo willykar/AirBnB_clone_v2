a587922189f3bb06e71bc7b3e4eaf4806172d07c
willykar <wilsonnjenga799@gmail.com>
2024-03-21 10:01:52 -0700

    update file

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -16,5 +15,5 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place",
-                                   secondary="place_amenity",
+                                   secondary=place_amenity,
                                    back_populates="amenities")
