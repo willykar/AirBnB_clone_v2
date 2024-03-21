bc6a2eb6645838f771f7342e8a50e18311075a72
willykar <wilsonnjenga799@gmail.com>
2024-03-21 07:23:16 -0700

    update

--- models/amenity.py
+++ models/amenity.py
@@ -16,5 +16,5 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place",
-                                   secondary=place_amenity,
+                                   secondary="place_amenity",
                                    back_populates="amenities")
--- models/place.py
+++ models/place.py
@@ -51,7 +51,7 @@ class Place(BaseModel, Base):
         reviews = relationship("Review",
                                cascade='all, delete, delete-orphan',
                                backref="place")
-        amenities = relationship("Amenity", secondary=place_amenity,
+        amenities = relationship("Amenity", secondary="place_amenity",
                                  viewonly=False)
     else:
         @property
