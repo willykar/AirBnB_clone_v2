f500276001ac95c573bb171ee0e87bd2ca93edf6
willykar <wilsonnjenga799@gmail.com>
2024-03-21 10:49:05 -0700

    update

--- models/place.py
+++ models/place.py
@@ -46,7 +46,6 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
 
     if getenv("BNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review",
