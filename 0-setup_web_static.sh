e3bd030986ca88a5a1bab5eec6a74404443f9745
willykar <103494595+willykar@users.noreply.github.com>
2024-04-05 03:26:11 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,16 +5,12 @@ sudo apt-get update
 sudo apt-get install -y nginx
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/release/test/
+
 sudo touch /data/web_static/releases/test/index.html
 
-sudo echo
-"
-<html>
+echo "<html>
   <head>
   </head>
   <body>
@@ -22,9 +18,9 @@ sudo echo
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-service nginx restart
+sudo systemctl restart nginx
